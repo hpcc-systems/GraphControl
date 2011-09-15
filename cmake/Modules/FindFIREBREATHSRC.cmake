79caa42f622e32d3ccc113296a1fb77efd95d3a3
Gordon Smith <gordonjsmith@gmail.com>
2011-09-15 12:48:03 +0100

    WIP:  Ubuntu building

---
+++ cmake/Modules/FindFIREBREATHSRC.cmake
@@ -0,0 +1,17 @@
+# - Try to find FIREBREATH
+# Once done this will define
+#  FIREBREATHSRC_FOUND - System has FIREBREATH
+#  FIREBREATHSRC_DIRS - The FIREBREATH include directories
+
+find_path(FIREBREATHSRC_DIR src/plugincore/plugincore.h
+          HINTS ${FIREBREATHSRC_ROOT} )
+
+set(FIREBREATHSRC_DIRS ${FIREBREATHSRC_DIR} )
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set FIREBREATH_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(FIREBREATHSRC DEFAULT_MSG
+                                  FIREBREATHSRC_DIR)
+
+mark_as_advanced( FIREBREATHSRC_DIRS )
