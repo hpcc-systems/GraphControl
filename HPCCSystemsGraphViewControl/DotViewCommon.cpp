9b25d250c36e4a199ce5a0c5b3e178e3742ddeee
Gordon Smith <gordon.smith@lexisnexis.com>
2011-12-18 07:15:44 +0000

    Add License
    Signed-off-by: Gordon Smith <gordon.smith@lexisnexis.com>

--- HPCCSystemsGraphViewControl/DotViewCommon.cpp
+++ HPCCSystemsGraphViewControl/DotViewCommon.cpp
@@ -1,6 +1,28 @@
+/*******************************************************************************
+ * Copyright (C) 2011 HPCC Systems.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
 #include "DotViewCommon.h"
 
 #include <XgmmlParser.h>
+#include <DotParser.h>
 #include <boost/algorithm/string.hpp> 
 #include <boost/format.hpp> 
 namespace boost
@@ -247,6 +269,15 @@ void CDotViewCommon::MergeXGMML(const std::string & xgmml)
 	std::string layout = m_g->GetPropertyString(ln::PROP_LAYOUT);
 	ln::MergeXGMML(m_g, xgmml);
 	Invalidate();
+}
+
+void CDotViewCommon::LoadDOT(const std::string & dot)
+{
+	std::string layout = m_g->GetPropertyString(ln::PROP_LAYOUT);
+	m_hotItem->Set(NULL);
+	m_selection->Clear();
+	m_g->Clear();
+	ln::LoadDOT(m_g, dot);
 }
 
 const char * CDotViewCommon::GetLocalisedXGMML(int _item, std::string & xgmml)
--- HPCCSystemsGraphViewControl/HPCCSystemsGraphViewControlAPI.cpp
+++ HPCCSystemsGraphViewControl/HPCCSystemsGraphViewControlAPI.cpp
@@ -1,3 +1,24 @@
+/*******************************************************************************
+ * Copyright (C) 2011 HPCC Systems.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
 #include "Platform.h"
 
 #include "JSObject.h"
@@ -33,6 +54,7 @@ HPCCSystemsGraphViewControlAPI::HPCCSystemsGraphViewControlAPI(const HPCCSystems
 	registerMethod("clear", make_method(this, &HPCCSystemsGraphViewControlAPI::clear));
 	registerMethod("loadXGMML", make_method(this, &HPCCSystemsGraphViewControlAPI::loadXGMML));
 	registerMethod("mergeXGMML", make_method(this, &HPCCSystemsGraphViewControlAPI::mergeXGMML));
+	registerMethod("loadDOT", make_method(this, &HPCCSystemsGraphViewControlAPI::loadDOT));
 	registerMethod("startLayout", make_method(this, &HPCCSystemsGraphViewControlAPI::startLayout));
 	registerMethod("setScale", make_method(this, &HPCCSystemsGraphViewControlAPI::setScale));
 	registerMethod("getScale", make_method(this, &HPCCSystemsGraphViewControlAPI::getScale));
@@ -142,6 +164,18 @@ bool HPCCSystemsGraphViewControlAPI::loadXGMML(const std::string& xgmml)
 bool HPCCSystemsGraphViewControlAPI::mergeXGMML(const std::string& xgmml)
 {
 	m_callback->MergeXGMML(xgmml);
+	return true;
+}
+
+bool HPCCSystemsGraphViewControlAPI::loadDOT(const std::string& dot)
+{
+    assert(m_callback != NULL);
+	if (dot.empty())
+	{
+		m_callback->Clear();
+		return true;
+	}
+	m_callback->LoadDOT(dot);
 	return true;
 }
 
