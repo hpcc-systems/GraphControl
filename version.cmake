d2914ea2657491ae4a7ac0c225e1ea3f31a6281a
Richard Chapman <rchapman@hpccsystems.com>
2013-04-17 17:34:19 +0100

    Split off 3.10.8
    Signed-off-by: Richard Chapman <rchapman@hpccsystems.com>

--- version.cmake
+++ version.cmake
@@ -4,8 +4,8 @@
 set ( HPCC_PROJECT "community" )
 set ( HPCC_MAJOR 3 )
 set ( HPCC_MINOR 10 )
-set ( HPCC_POINT 8 )
-set ( HPCC_MATURITY "rc" )
+set ( HPCC_POINT 9 )
+set ( HPCC_MATURITY "closedown" )
 set ( HPCC_SEQUENCE 1 )
 ###
 IF ( ${HPCC_POINT} GREATER 65 )
